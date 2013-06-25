652659974e7c1f0d2f6c10a504000c9bc3874d7a
Herve Peroteau <herve.peroteau@gmail.com>
2013-06-25 21:24:16 +0200

    Initial Commit

---
+++ HPYoutube/HPYoutube.m
@@ -0,0 +1,13 @@
+//
+//  HPYoutube.m
+//  HPYoutube
+//
+//  Created by Hervé PEROTEAU on 25/06/13.
+//  Copyright (c) 2013 Hervé PEROTEAU. All rights reserved.
+//
+
+#import "HPYoutube.h"
+
+@implementation HPYoutube
+
+@end
---
+++ HPYoutubeTests/HPYoutubeTests.m
@@ -0,0 +1,36 @@
+//
+//  HPYoutubeTests.m
+//  HPYoutubeTests
+//
+//  Created by Hervé PEROTEAU on 25/06/13.
+//  Copyright (c) 2013 Hervé PEROTEAU. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HPYoutubeTests : XCTestCase
+
+@end
+
+@implementation HPYoutubeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
